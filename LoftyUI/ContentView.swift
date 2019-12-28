ee2cb52528c1983eca02c4d0abbb5ea6d27a2368
Oliver Lippold <olippold@hotmail.com>
2019-12-28 15:20:52 +0000

    Initial Commit

---
+++ LoftyUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoftyUI
+//
+//  Created by Oliver Lippold on 28/12/2019.
+//  Copyright © 2019 Oliver Lippold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
